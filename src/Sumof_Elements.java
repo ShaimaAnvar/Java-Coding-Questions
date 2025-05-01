373c332fdf92633bd747fffb3f78b3c35210c638
Shaima Anvar <shaima.anvar@gmail.com>
2025-05-01 17:14:04 -0400

    Sum of elements in an array

---
+++ src/Sumof_Elements.java
@@ -0,0 +1,26 @@
+
+//Create a method which accepts an array and returns sum of all elements in an array
+
+//public class Sumof_Elements {
+//    public static void main(String[] args){
+//        int[] a = {1,3,4,2,6};
+//        int sum= sumArray(a);
+//
+//        System.out.println(sum);
+//    }
+//    public static int sumArray(int[] a){
+//        int sum=0;
+//        for(int i=0; i<a.length;i++ ){
+//            sum=sum+a[i];
+//        }
+//        return sum;
+//    }
+//}
+
+
+
+
+
+
+
+
